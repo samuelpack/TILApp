0f119a32e47ffa464d5d1834005ed76f019db6e1
samuelpack <packmac2@gmail.com>
2019-08-23 13:59:46 -0400

    good thru chapter 11

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -20,11 +20,24 @@ public func configure(
     var databases = DatabaseConfig()
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
-    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    
+    //configured to allow testing on a db OTHER than production
+    let databaseName: String
+    let databasePort: Int
+    
+    if (env == .testing) {
+        databaseName = "vapor-test"
+        databasePort = 5433
+    } else {
+        databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        databasePort = 5432
+    }
+    
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     
     let databaseConfig = PostgreSQLDatabaseConfig (
         hostname: hostname,
+        port: databasePort,
         username: username,
         database: databaseName,
         password: password)
